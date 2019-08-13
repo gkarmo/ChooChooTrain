f326f9d93e88bab27411c530d94b0549be8d2187
Gayah Karmo <gayahkarmo@gmail.com>
2019-08-13 14:59:56 -0400

    table set up

--- app.js
+++ app.js
@@ -1,37 +1,48 @@
+var moment = require("moment");
+
 $(document).ready(function() {
+  // create table
+  var $table = $("<table>").addClass("table");
+  $table
+    // thead
+    .append("<thead>")
+    .children("thead")
+    .append("<tr />")
+    .children("tr")
+    .append(
+      "<th>Train Name</th><th>Destination</th><th>Frequency</th><th>Next Arrival</th><th>Mins Away</th>"
+    );
+
+  $table.appendTo("#trains");
+
   $("#add-train").on("click", function(event) {
     console.log("click");
-    var name = $("#train-name").val().trim();
-    var destination = $("#destination").val().trim();
+    var name = $("#train-name")
+      .val()
+      .trim();
+    var destination = $("#destination")
+      .val()
+      .trim();
+    var frequency = $("#frequency")
+      .val()
+      .trim();
 
+    //tbody
+    var $tbody = $table.append("<tbody />").children("tbody");
 
-
-// create table
-var $table = $('<table>');
-// caption
-$table.append('<caption>MyTable</caption>')
-// thead
-.append('<thead>').addClass("thead-dark").children('thead')
-.append('<tr />').children('tr').append('<th>Train Name</th><th>Destination</th>');
-
-//tbody
-var $tbody = $table.append('<tbody />').children('tbody');
-
-// add row
-$tbody.append('<tr />').children('tr:last')
-.append("<td>" + name + "</td>")
-.append("<td>" + destination + "</td>");
-
-
-
-// add table to dom
-$table.appendTo('#trains');
- 
-
-    //   .append("<td>" + name + "</td>")
-    //   .append("<td>" + destination + "</td>");
+    // add row
+    $tbody
+      .append("<tr />")
+      .children("tr:last")
+      .append("<td>" + name + "</td>")
+      .append("<td>" + destination + "</td>")
+      .append("<td>" + frequency + "</td>");
 
     $("#train-name").val("");
     $("#destination").val("");
+    $("#frequency").val("");
+
+
+
   });
 });
