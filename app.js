ebef7ef4d04c1be47a8aa9b0e419f4e2b8a6abb5
Gayah Karmo <gayahkarmo@gmail.com>
2019-09-17 18:18:44 -0400

    click button refuses to work

--- app.js
+++ app.js
@@ -1,44 +1,45 @@
-// // Your web app's Firebase configuration
-// var firebaseConfig = {
-//   apiKey: "AIzaSyBnA4nnPlyvzqiaMzwAfzbN_HMS6C11uf8",
-//   authDomain: "choochootrain-17a0a.firebaseapp.com",
-//   databaseURL: "https://choochootrain-17a0a.firebaseio.com",
-//   projectId: "choochootrain-17a0a",
-//   storageBucket: "",
-//   messagingSenderId: "732377568440",
-//   appId: "1:732377568440:web:f8573634369e4c6a5bd04d"
-// };
-// // Initialize Firebase
-// firebase.initializeApp(firebaseConfig);
+// Your web app's Firebase configuration
+var firebaseConfig = {
+  apiKey: "AIzaSyBnA4nnPlyvzqiaMzwAfzbN_HMS6C11uf8",
+  authDomain: "choochootrain-17a0a.firebaseapp.com",
+  databaseURL: "https://choochootrain-17a0a.firebaseio.com",
+  projectId: "choochootrain-17a0a",
+  storageBucket: "",
+  messagingSenderId: "732377568440",
+  appId: "1:732377568440:web:f8573634369e4c6a5bd04d"
+};
+// Initialize Firebase
+firebase.initializeApp(firebaseConfig);
 
-// var database = firebase.database();
+var database = firebase.database();
 
 $("#add-train").on("click", function(event) {
   event.preventDefault();
 
   // Grabs user input
   var tName = $("#train-name").val().trim();
-  var destination = $("#destination").val().trim();
+  // var destination = $("#destination").val().trim();
   // var firstTrain = moment($("#time").val().trim(), "MM/DD/YYYY").format("X");
-  var frequency = $("#frequency").val().trim();
+  // var frequency = $("#frequency").val().trim();
 
   // Creates local "temporary" object for holding employee data
-  // var newTrain = {
-  //   name: tName,
-  //   dest: destination,
-  //   // firstT: firstTrain,
-  //   freq: frequency
-  // };
+  var newTrain = {
+    name: tName
+    // dest: destination,
+    // firstT: firstTrain,
+    // freq: frequency
+  };
 
-  // database.ref().push(newTrain);
+  database.ref().push(newTrain);
 
-  console.log(tName);
-  console.log(destination);
+  console.log(tName.name);
+  // console.log(destination);
   // console.log(firstTrain.firstT);
-  console.log(frequency);
+  // console.log(frequency);
 
   alert("Train successfully added");
   
+  $("#train-name").val("");
 
 
 });
