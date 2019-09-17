43f58991c7ee3920ce92f22156e3ebf50cc3ef6a
Gayah Karmo <gayahkarmo@gmail.com>
2019-09-17 18:02:21 -0400

    fooling with firebase

--- app.js
+++ app.js
@@ -1,55 +1,44 @@
+// // Your web app's Firebase configuration
+// var firebaseConfig = {
+//   apiKey: "AIzaSyBnA4nnPlyvzqiaMzwAfzbN_HMS6C11uf8",
+//   authDomain: "choochootrain-17a0a.firebaseapp.com",
+//   databaseURL: "https://choochootrain-17a0a.firebaseio.com",
+//   projectId: "choochootrain-17a0a",
+//   storageBucket: "",
+//   messagingSenderId: "732377568440",
+//   appId: "1:732377568440:web:f8573634369e4c6a5bd04d"
+// };
+// // Initialize Firebase
+// firebase.initializeApp(firebaseConfig);
 
-  // Your web app's Firebase configuration
-  var firebaseConfig = {
-    apiKey: "AIzaSyBnA4nnPlyvzqiaMzwAfzbN_HMS6C11uf8",
-    authDomain: "choochootrain-17a0a.firebaseapp.com",
-    databaseURL: "https://choochootrain-17a0a.firebaseio.com",
-    projectId: "choochootrain-17a0a",
-    storageBucket: "",
-    messagingSenderId: "732377568440",
-    appId: "1:732377568440:web:f8573634369e4c6a5bd04d"
-  };
-  // Initialize Firebase
-  firebase.initializeApp(firebaseConfig);
+// var database = firebase.database();
 
-  var db = firebase.database();
+$("#add-train").on("click", function(event) {
+  event.preventDefault();
 
-$(document).ready(function() {
+  // Grabs user input
+  var tName = $("#train-name").val().trim();
+  var destination = $("#destination").val().trim();
+  // var firstTrain = moment($("#time").val().trim(), "MM/DD/YYYY").format("X");
+  var frequency = $("#frequency").val().trim();
 
+  // Creates local "temporary" object for holding employee data
+  // var newTrain = {
+  //   name: tName,
+  //   dest: destination,
+  //   // firstT: firstTrain,
+  //   freq: frequency
+  // };
 
-  $("#add-train").on("click", function(event) {
-    console.log("click");
+  // database.ref().push(newTrain);
 
-    
-    var firstTrain = '';
-    var nextArrival = moment().startOf(firstTrain).fromNow();
+  console.log(tName);
+  console.log(destination);
+  // console.log(firstTrain.firstT);
+  console.log(frequency);
 
-    var name = $("#train-name")
-      .val()
-      .trim();
-    var destination = $("#destination")
-      .val()
-      .trim();
-    var frequency = $("#frequency")
-      .val()
-      .trim();
-
-    //tbody
-    var $tbody = $table.append("<tbody />").children("tbody");
-
-    // add row
-    $tbody
-      .append("<tr />")
-      .children("tr:last")
-      .append("<td>" + name + "</td>")
-      .append("<td>" + destination + "</td>")
-      .append("<td>" + frequency + "</td>");
-
-    $("#train-name").val("");
-    $("#destination").val("");
-    $("#frequency").val("");
+  alert("Train successfully added");
+  
 
 
-
-  });
 });
